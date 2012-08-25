896a824776dbc9ba7156b0b4a1395cfd48fd0375
Leonardo Campos <leonardoscampos@gmail.com>
2012-08-25 00:35:26 -0300

    Creating 'CompiledExpression' class (SoC)

--- SimpleExpressionEvaluator/ExpressionEvaluator.cs
+++ SimpleExpressionEvaluator/ExpressionEvaluator.cs
@@ -1,96 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 
 namespace SimpleExpressionEvaluator
 {
-    public class ExpressionEvaluator : DynamicObject
+    public class ExpressionEvaluator
     {
-        private readonly string decimalSeparator;
-        private readonly Stack<Expression> expressionStack;
-        private readonly Stack<char> operatorStack;
-        private readonly List<string> parameters;
+        #region Fields
+
+        private readonly string _decimalSeparator;
+
+        #endregion
+
+        #region Constrcutors
 
         public ExpressionEvaluator()
         {
-            decimalSeparator = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator;
-            expressionStack = new Stack<Expression>();
-            operatorStack = new Stack<char>();
-            parameters = new List<string>();
+            _decimalSeparator = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator;
         }
 
-        public Func<object, decimal> Compile(string expression)
-        {
-            var compiled = Parse(expression);
-
-            Func<object, decimal> result = argument =>
-            {
-                var arguments = ParseArguments(argument);
-                return Execute(compiled, arguments);
-            };
-
-            return result;
-        }
-
-        public decimal Evaluate(string expression, object argument = null)
-        {
-            var arguments = ParseArguments(argument);
-
-            return Evaluate(expression, arguments);
-        }
-
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
-        {
-            if ("Evaluate" != binder.Name)
-            {
-                return base.TryInvokeMember(binder, args, out result);
-            }
-
-            if (!(args[0] is string))
-            {
-                throw new ArgumentException("No expression specified for parsing");
-            }
-
-            //args will contain expression and arguments,
-            //ArgumentNames will contain only named arguments
-            if (args.Length != binder.CallInfo.ArgumentNames.Count + 1)
-            {
-                throw new ArgumentException("Argument names missing.");
-            }
-
-            var arguments = new Dictionary<string, decimal>();
-
-            for (int i = 0; i < binder.CallInfo.ArgumentNames.Count; i++)
-            {
-                if (IsNumeric(args[i + 1].GetType()))
-                {
-                    arguments.Add(binder.CallInfo.ArgumentNames[i], Convert.ToDecimal(args[i + 1]));
-                }
-            }
-
-            result = Evaluate((string)args[0], arguments);
-
-            return true;
-        }
+        #endregion
 
 
-        private Func<decimal[], decimal> Parse(string expression)
+        #region Private Methods
+
+        private CompiledExpression Parse(string expression)
         {
             if (string.IsNullOrWhiteSpace(expression))
             {
-                return s => 0;
+                return new CompiledExpression(s => 0, new List<string>());
             }
 
             var arrayParameter = Expression.Parameter(typeof(decimal[]), "args");
 
-            parameters.Clear();
-            operatorStack.Clear();
-            expressionStack.Clear();
+            var expressionStack = new Stack<Expression>();
+            var operatorStack = new Stack<char>();
+            var parameters = new List<string>();
 
             using (var reader = new StringReader(expression))
             {
@@ -107,7 +54,7 @@ namespace SimpleExpressionEvaluator
 
                     if (char.IsLetter(next))
                     {
-                        expressionStack.Push(ReadParameter(reader, arrayParameter));
+                        expressionStack.Push(ReadParameter(reader, arrayParameter, parameters));
                         continue;
                     }
 
@@ -115,8 +62,10 @@ namespace SimpleExpressionEvaluator
                     {
                         var currentOperation = ReadOperation(reader);
 
-                        EvaluateWhile(() => operatorStack.Count > 0 && operatorStack.Peek() != '(' &&
-                            currentOperation.Precedence <= ((Operation)operatorStack.Peek()).Precedence);
+                        EvaluateWhile(() => operatorStack.Count > 0 &&
+                                            operatorStack.Peek() != '(' &&
+                                            currentOperation.Precedence <= ((Operation) operatorStack.Peek()).Precedence,
+                                      expressionStack, operatorStack);
 
                         operatorStack.Push(next);
                         continue;
@@ -132,7 +81,9 @@ namespace SimpleExpressionEvaluator
                     if (next == ')')
                     {
                         reader.Read();
-                        EvaluateWhile(() => operatorStack.Count > 0 && operatorStack.Peek() != '(');
+                        EvaluateWhile(() => operatorStack.Count > 0 && operatorStack.Peek() != '(',
+                                      expressionStack, operatorStack);
+
                         operatorStack.Pop();
                         continue;
                     }
@@ -149,62 +100,15 @@ namespace SimpleExpressionEvaluator
                 }
             }
 
-            EvaluateWhile(() => operatorStack.Count > 0);
+            EvaluateWhile(() => operatorStack.Count > 0, expressionStack, operatorStack);
 
             var lambda = Expression.Lambda<Func<decimal[], decimal>>(expressionStack.Pop(), arrayParameter);
             var compiled = lambda.Compile();
-            return compiled;
+
+            return new CompiledExpression(compiled, parameters);
         }
 
-        private Dictionary<string, decimal> ParseArguments(object argument)
-        {
-            if (argument == null)
-            {
-                return new Dictionary<string, decimal>();
-            }
-
-            var argumentType = argument.GetType();
-
-            var properties = argumentType.GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                .Where(p => p.CanRead && IsNumeric(p.PropertyType));
-
-            var arguments = properties.ToDictionary(property => property.Name,
-                property => Convert.ToDecimal(property.GetValue(argument, null)));
-
-            return arguments;
-        }
-
-        private decimal Evaluate(string expression, Dictionary<string, decimal> arguments)
-        {
-            var compiled = Parse(expression);
-
-            return Execute(compiled, arguments);
-        }
-
-        private decimal Execute(Func<decimal[], decimal> compiled, Dictionary<string, decimal> arguments)
-        {
-            arguments = arguments ?? new Dictionary<string, decimal>();
-
-            if (parameters.Count != arguments.Count)
-            {
-                throw new ArgumentException(string.Format("Expression contains {0} parameters but got only {1}",
-                    parameters.Count, arguments.Count));
-            }
-
-            var missingParameters = parameters.Where(p => !arguments.ContainsKey(p)).ToList();
-
-            if (missingParameters.Any())
-            {
-                throw new ArgumentException("No values provided for parameters: " + string.Join(",", missingParameters));
-            }
-
-            var values = parameters.Select(parameter => arguments[parameter]).ToArray();
-
-            return compiled(values);
-        }
-
-
-        private void EvaluateWhile(Func<bool> condition)
+        private void EvaluateWhile(Func<bool> condition, Stack<Expression> expressionStack, Stack<char> operatorStack)
         {
             while (condition())
             {
@@ -233,7 +137,7 @@ namespace SimpleExpressionEvaluator
                 else if ((next == '.') || (next == ','))
                 {
                     reader.Read();
-                    operand += decimalSeparator;
+                    operand += _decimalSeparator;
                 }
                 else
                 {
@@ -250,7 +154,7 @@ namespace SimpleExpressionEvaluator
             return (Operation)operation;
         }
 
-        private Expression ReadParameter(TextReader reader, Expression arrayParameter)
+        private Expression ReadParameter(TextReader reader, Expression arrayParameter, List<string> parameters)
         {
             var parameter = string.Empty;
 
@@ -279,81 +183,73 @@ namespace SimpleExpressionEvaluator
             return Expression.ArrayIndex(arrayParameter, Expression.Constant(parameters.IndexOf(parameter)));
         }
 
+        #endregion
 
-        private bool IsNumeric(Type type)
+        #region Public Methods
+
+        public CompiledExpression Compile(string expression)
         {
-            switch (Type.GetTypeCode(type))
+            return Parse(expression);
+        }
+
+        #endregion
+
+        #region Inner Classes
+
+        private sealed class Operation
+        {
+            private readonly int _precedence;
+            private readonly Func<Expression, Expression, Expression> _operation;
+
+            private static readonly Dictionary<char, Operation> Operations;
+
+            static Operation()
             {
-                case TypeCode.SByte:
-                case TypeCode.Byte:
-                case TypeCode.Int16:
-                case TypeCode.UInt16:
-                case TypeCode.Int32:
-                case TypeCode.UInt32:
-                case TypeCode.Int64:
-                case TypeCode.UInt64:
-                case TypeCode.Single:
-                case TypeCode.Double:
-                case TypeCode.Decimal:
-                    return true;
+                Operations = new Dictionary<char, Operation>
+                                 {
+                                     {'+', new Operation(1, Expression.Add)},
+                                     {'-', new Operation(1, Expression.Subtract)},
+                                     {'*', new Operation(2, Expression.Multiply)},
+                                     {'/', new Operation(2, Expression.Divide)}
+                                 };
             }
-            return false;
-        }
-    }
 
-    internal sealed class Operation
-    {
-        private readonly int precedence;
-        private readonly string name;
-        private readonly Func<Expression, Expression, Expression> operation;
-
-        public static readonly Operation Addition = new Operation(1, Expression.Add, "Addition");
-        public static readonly Operation Subtraction = new Operation(1, Expression.Subtract, "Subtraction");
-        public static readonly Operation Multiplication = new Operation(2, Expression.Multiply, "Multiplication");
-        public static readonly Operation Division = new Operation(2, Expression.Divide, "Division");
-
-        private static readonly Dictionary<char, Operation> Operations = new Dictionary<char, Operation>
-        {
-            { '+', Addition },
-            { '-', Subtraction },
-            { '*', Multiplication},
-            { '/', Division }
-        };
-
-        private Operation(int precedence, Func<Expression, Expression, Expression> operation, string name)
-        {
-            this.precedence = precedence;
-            this.operation = operation;
-            this.name = name;
-        }
-
-        public int Precedence
-        {
-            get { return precedence; }
-        }
-
-        public static explicit operator Operation(char operation)
-        {
-            Operation result;
-
-            if (Operations.TryGetValue(operation, out result))
+            private Operation(int precedence, Func<Expression, Expression, Expression> operation)
             {
-                return result;
+                _precedence = precedence;
+                _operation = operation;
             }
-            else
+
+            public int Precedence
             {
-                throw new InvalidCastException();
+                get { return _precedence; }
+            }
+
+            public static explicit operator Operation(char operation)
+            {
+                Operation result;
+
+                if (Operations.TryGetValue(operation, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    throw new InvalidCastException();
+                }
+            }
+
+            public Expression Apply(Expression left, Expression right)
+            {
+                return _operation(left, right);
+            }
+
+            public static bool IsDefined(char operation)
+            {
+                return Operations.ContainsKey(operation);
             }
         }
 
-        public Expression Apply(Expression left, Expression right)
-        {
-            return operation(left, right);
-        }
-
-        public static bool IsDefined(char operation)
-        {
-            return Operations.ContainsKey(operation);
-        }
+        #endregion
     }
 }
